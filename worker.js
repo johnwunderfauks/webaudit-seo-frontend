461028fca4c49a27d627a1934f76c4a6e90cd1a5
jiaxiwunderfauks <jiaxi@wunderfauks.com>
2023-01-30 18:24:50 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -9,7 +9,7 @@ require("dotenv").config({ path: `.env`, override: true });
 
 //log.e("ln"); //Android-like logging signature:
 const axios = require("axios");
-const puppeteer = require("puppeteer-core");
+const puppeteer = require("puppeteer");
 const lighthouse = require("lighthouse");
 const { URL } = require("url");
 const { format } = require("date-fns");
